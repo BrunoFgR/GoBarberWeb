bc791dea09ed6b1be05baf49b5b04016c6ff2298
BrunoFgR <brunofds281@gmail.com>
2019-10-16 00:22:26 -0300

    projeto completo

---
+++ src/store/modules/auth/sagas.js
@@ -0,0 +1,74 @@
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSucess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
+
+    const { user, token } = response.data;
+
+    if (!user.provider) {
+      toast.error('O usuário não é prestador');
+      return;
+    }
+
+    yield put(signInSucess(token, user));
+
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    history.push('/dashboard');
+  } catch (error) {
+    toast.error('Falha na autentificação, verifique os seus dados');
+    yield put(signFailure());
+  }
+}
+
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (error) {
+    toast.error('Falha no cadastro, verifique os seus dados');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+export function signOut() {
+  history.push('/');
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+  takeLatest('@auth/SIGN_OUT', signOut),
+]);
